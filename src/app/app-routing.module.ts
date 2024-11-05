251c4ad1f557fb40b64bd94f0c926ce3f511abc4
kacem <kacemmeherzi52@gmail.com>
2024-11-05 10:57:47 +0100

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnnonceListComponent } from './components/annonce-list/annonce-list.component';
+import { AnnonceFormComponent } from './annonce-form/annonce-form.component';
+import { AnnonceDetailsComponent } from './annonce-details/annonce-details.component';
 
 const routes: Routes = [
   { path: 'annonces', component: AnnonceListComponent },
-  { path: '', redirectTo: '/annonces', pathMatch: 'full' }
+  { path: 'create-annonce', component: AnnonceFormComponent },
+  { path: 'details/:id', component: AnnonceDetailsComponent }, // Route for details
+
+
+  { path: '', redirectTo: '/annonces', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
